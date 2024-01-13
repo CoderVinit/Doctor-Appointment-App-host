af9908fd98cce9d3756bf29113ed8ba0228f84ff
CoderVinit <vinitpatel8896@gmail.com>
2024-01-13 16:18:34 +0530

    final commit

---
+++ server/index.js
@@ -0,0 +1,40 @@
+import express from "express"
+import dotenv from 'dotenv'
+import Connection from "./dbConfig.js";
+import userRoute from './Routes/userRoutes.js'
+import cors from 'cors'
+import bodyParser from 'body-parser';
+import adminRoute from './Routes/adminRoutes.js'
+import doctorRoute from './Routes/doctorRoutes.js'
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
+dotenv.config();
+
+
+app.get("/", (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "client", "build")));
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
+
+
+
+const port = process.env.port;
+
+
+app.use('/api/user', userRoute);
+app.use('/api/admin', adminRoute);
+app.use('/api/doctor', doctorRoute);
+
+Connection().then(
+  app.listen(port, () => console.log(`this server started at port ${port}`)));
